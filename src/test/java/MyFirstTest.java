5fdb1ef5bd2d27a205a0663affa0378dd9a8d620
VitaliyMalinovskiy <vm122345@gmail.com>
2017-05-24 15:04:29 +0300

    First commit

---
+++ src/test/java/MyFirstTest.java
@@ -0,0 +1,35 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
+
+public class MyFirstTest {
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    @Before
+    public void start() {
+        driver = new FirefoxDriver();
+        wait = new WebDriverWait(driver, 10);
+    }
+
+    @Test
+    public void MyFirstTest() {
+        driver.get("http:/www.google.com");
+        driver.findElement(By.name("q")).sendKeys("webdriver");
+        driver.findElement(By.name("btnG")).click();
+        wait.until(titleIs("webdriver - Поиск в Google"));
+    }
+
+    @After
+    public void stop() {
+        driver.quit();
+        driver = null;
+
+    }
+}
