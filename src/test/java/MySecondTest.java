616abe29663f43ebcd264597458d4d7384501e86
VitaliyMalinovskiy <vm122345@gmail.com>
2017-06-05 18:24:55 +0300

    next commit

--- src/test/java/MySecondTest.java
+++ src/test/java/MySecondTest.java
@@ -32,36 +32,34 @@ public class MySecondTest {
         loginTest();
 
         driver.findElements(By.cssSelector("ul#box-apps-menu li#app-")).get(0).click();
-        driver.findElement(By.cssSelector("li#app-.selected"));
-        driver.findElement(By.cssSelector("ul.docs li#doc-template")).click();
-        driver.findElement(By.cssSelector("ul.docs li#doc-logotype")).click();
+        driver.findElement(By.cssSelector("li#doc-template.selected"));
+        driver.findElement(By.cssSelector("li#doc-logotype")).click();
 
         driver.findElements(By.cssSelector("ul#box-apps-menu li#app-")).get(1).click();
-        driver.findElement(By.cssSelector("li#app-.selected"));
-        driver.findElement(By.cssSelector("ul.docs li#doc-catalog"));
-        driver.findElement(By.cssSelector("ul.docs li#doc-product_groups")).click();
-        driver.findElement(By.cssSelector("ul.docs li#doc-manufacturers")).click();
-        driver.findElement(By.cssSelector("ul.docs li#doc-suppliers")).click();
-        driver.findElement(By.cssSelector("ul.docs li#doc-delivery_statuses")).click();
-        driver.findElement(By.cssSelector("ul.docs li#doc-sold_out_statuses")).click();
-        driver.findElement(By.cssSelector("ul.docs li#doc-quantity_units")).click();
-//        driver.findElement(By.partialLinkText("app=catalog&doc-csv")).click();
+        driver.findElement(By.cssSelector("li#doc-catalog.selected"));
+        driver.findElement(By.cssSelector("li#doc-product_groups")).click();
+        driver.findElement(By.cssSelector("li#doc-manufacturers")).click();
+        driver.findElement(By.cssSelector("li#doc-suppliers")).click();
+        driver.findElement(By.cssSelector("li#doc-delivery_statuses")).click();
+        driver.findElement(By.cssSelector("li#doc-sold_out_statuses")).click();
+        driver.findElement(By.cssSelector("li#doc-quantity_units")).click();
+        driver.findElement(By.cssSelector("li#doc-csv")).click();
 
 
         driver.findElements(By.cssSelector("ul#box-apps-menu li#app-")).get(2).click();
         driver.findElement(By.cssSelector("li#app-.selected"));
 
         driver.findElements(By.cssSelector("ul#box-apps-menu li#app-")).get(3).click();
-        driver.findElement(By.cssSelector("li#app-.selected"));
-//        driver.findElement(By.cssSelector("li#app- ul.docs li#doc-csv")).click();
-//        driver.findElement(By.cssSelector("li#app- ul.docs li#doc-newsletter")).click();
+        driver.findElement(By.cssSelector("li#doc-customer.selected")).click();
+        driver.findElement(By.cssSelector("li#doc-csv")).click();
+        driver.findElement(By.cssSelector("li#doc-newsletter")).click();
 
         driver.findElements(By.cssSelector("ul#box-apps-menu li#app-")).get(4).click();
         driver.findElement(By.cssSelector("li#app-.selected"));
 
         driver.findElements(By.cssSelector("ul#box-apps-menu li#app-")).get(5).click();
         driver.findElement(By.cssSelector("li#app-.selected"));
-//        driver.findElement(By.cssSelector("li#app- ul.docs li#doc-storage_encoding")).click();
+        driver.findElement(By.xpath(".//*[@id='doc-csv']/a/span")).click();
 
         driver.findElements(By.cssSelector("ul#box-apps-menu li#app-")).get(6).click();
         driver.findElement(By.cssSelector("li#app-.selected"));
