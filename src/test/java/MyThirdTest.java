7c46dca0dbf200454bec3127aad4b454851d6fdb
VitaliyMalinovskiy <vm122345@gmail.com>
2017-06-07 14:16:34 +0300

    Next commit

--- src/test/java/MyThirdTest.java
+++ src/test/java/MyThirdTest.java
@@ -1,7 +1,34 @@
-package PACKAGE_NAME;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by malinovskiyv on 06.06.2017.
  */
 public class MyThirdTest {
+    private WebDriver driver;
+
+    @Before
+    public void start() {
+        driver = new FirefoxDriver(new DesiredCapabilities());
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+    public void loginTest() {
+        driver.get("http://localhost/litecart/admin");
+        driver.findElement(By.cssSelector("input[name=username]")).sendKeys("admin");
+        driver.findElement(By.cssSelector("input[name=password]")).sendKeys("admin");
+        driver.findElement(By.cssSelector("button[name=login]")).click();
+    }
+
+    @Test
+    public void checkCountries() {
+        loginTest();
+        driver.get("http://localhost/litecart/admin/?app=countries&doc=countries");
+        driver.findElement(By.xpath(".//*[@id='content']/form/table/tbody"));
+    }
 }
