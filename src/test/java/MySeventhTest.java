c5fc2b92d0a4a4d39289f388e373556f0d427e6f
VitaliyMalinovskiy <vm122345@gmail.com>
2017-06-19 18:34:32 +0300

    Next commit

--- src/test/java/MySeventhTest.java
+++ src/test/java/MySeventhTest.java
@@ -1,14 +1,15 @@
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.util.concurrent.TimeUnit;
+import java.util.List;
 
 /**
  * Created by Vitaliy on 6/17/2017.
@@ -16,44 +17,45 @@ import java.util.concurrent.TimeUnit;
 public class MySeventhTest {
     private WebDriver driver;
     private WebDriverWait wait;
+    private WebElement element;
 
     @Before
     public void start() {
-        driver = new FirefoxDriver();
+        driver = new ChromeDriver();
         wait = new WebDriverWait(driver, 20);
     }
 
     @Test
     public void checkRecycle() {
-//.product - любая уточка
-// .//*[@id='cart']/a[2]/span[1] - цифирка
-        addProduct();
-        addProduct();
-        addProduct();
-        addProduct();
-        addProduct();
-        String textContent = driver.findElement(By.xpath(".//*[@id='cart']/a[2]/span[1]")).getAttribute("textContent");
-        System.out.println(textContent);
-//2) открыть первый товар из списка
-//2) добавить его в корзину (при этом может случайно добавиться товар, который там уже есть, ничего страшного)
-//3) подождать, пока счётчик товаров в корзине обновится
-//4) вернуться на главную страницу, повторить предыдущие шаги ещё два раза, чтобы в общей сложности в корзине было 3 единицы товара
-//5) открыть корзину (в правом верхнем углу кликнуть по ссылке Checkout)
-//6) удалить все товары из корзины один за другим, после каждого удаления подождать, пока внизу обновится таблица
+//        добавить объекты
+        addElementAndCheck("1");
+        addElementAndCheck("2");
+        addElementAndCheck("3");
+//        удалить боъекты
+        driver.get("http://localhost/litecart/");
+        driver.findElement(By.cssSelector("div#cart > a.link")).click();
+        wait.until(driver1 -> driver1.findElement(By.cssSelector("#box-checkout-summary > h2")));
+        driver.findElements(By.cssSelector("#order_confirmation-wrapper > table > tbody > tr"));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(("#box-checkout-cart > div > ul > li:nth-child(1) > form > div > p:nth-child(4) > button"))));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(("#box-checkout-cart > div > ul > li:nth-child(2) > form > div > p:nth-child(4) > button"))));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(("#box-checkout-cart > div > ul > li:nth-child(3) > form > div > p:nth-child(4) > button"))));
+        System.out.println("WOW");
     }
 
-    private void addProduct() {
-        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-        driver.get("http://localhost/litecart/en/");
-        WebElement until = wait.until(driver1 -> driver.findElement(By.cssSelector(".content .link")));
-        Actions action = new Actions(driver);
-        action.moveToElement(until).click().perform();
-        WebElement until1 = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(".quantity>button")));
-        action.moveToElement(until1).click().perform();
-        until1.click();
+    private void addElementAndCheck(String s) {
+        driver.get("http://localhost/litecart/");
+        WebElement element = driver.findElement(By.cssSelector("#box-most-popular > div > ul > li:nth-child(1)"));
+        element.click();
+        List<WebElement> elements = wait.until(driver1 -> driver.findElements(By.cssSelector("form[name=buy_now_form]>table>tbody>tr")));
+        if (elements.size() > 1) {
+            Select select = new Select(driver.findElement(By.cssSelector("#box-product > div.content > div.information > div.buy_now > form > table > tbody > tr:nth-child(1) > td > select")));
+            select.selectByIndex(1);
+        }
+        elements.get(elements.size() - 1).findElement(By.cssSelector("button[name=add_cart_product]")).click();
+        wait.until(ExpectedConditions.attributeContains(driver.findElement(By.cssSelector("#cart > a.content > span.quantity")), "innerText", s));
     }
 
-    //    @After
+    @After
     public void close() {
         driver.close();
         driver = null;
